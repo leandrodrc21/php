1ed60a9e3f7e7e1252b0f29715a2f74561d3c7e2
leandrodrc21 <51094309+leandrodrc21@users.noreply.github.com>
2020-06-23 14:01:27 -0500

    Update config.php
    sadasd

--- config.php
+++ config.php
@@ -7,6 +7,13 @@ $user = getenv("databaseuser");
 $pass = getenv("databasepassword");
 $db =   getenv("databasename");
 
+ 
+echo  $host;
+echo  $port;
+echo  $user;
+echo  $pass;
+echo  $db;
+
 $con=mysqli_connect($host,$user,$pass,$db);
 
 if(!$con)
@@ -16,7 +23,7 @@ if(!$con)
 }
 else
 {
-	//echo("Connected");
+	echo("Connected");
 }
 
 
